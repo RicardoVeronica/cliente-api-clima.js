0cc60ce8fb92fbf8d6e79c9e30bf7f9722a126aa
Ricardo Veronica <charlie.bronson00@gmail.com>
2020-12-29 10:59:07 -0600

    Update consultarAPI.js

--- src/functions/consultarAPI.js
+++ src/functions/consultarAPI.js
@@ -5,7 +5,7 @@ import mostrarSpinner from "./mostrarSpinner";
 
 function consultarAPI(ciudad, pais) {
   // Consulta la API para regresar resultado
-  const apiKey = "90c51111fe00e6f724b8e1310ca79d96";
+  const apiKey = "xxx";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiKey}`;
 
   mostrarSpinner();
